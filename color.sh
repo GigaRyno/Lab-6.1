923696e9c4cd8367d6e4a9464c97438097163cff
Ryan LaVigne <lavigner@pop-os.localdomain>
2020-10-13 20:24:11 -0400

    Lab 6.1 created and finished

---
+++ color.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+default='\033[0m'
+
+read -p "What is your favorite color (red, green, blue, yellow, magenta)?" color
+color=${color^^}
+
+case $color in 
+    "RED") selected_color=$red 
+    echo -e "${selected_color}Your selected color is ${color}";;
+    "GREEN") selected_color=$green
+    echo -e "${selected_color}Your selected color is ${color}";;
+    "YELLOW") selected_color=$yellow 
+    echo -e "${selected_color}Your selected color is ${color}";;
+    "BLUE") selected_color=$blue
+    echo -e "${selected_color}Your selected color is ${color}";;
+    "MAGENTA") selected_color=$magenta
+    echo -e "${selected_color}Your selected color is ${color}";;
+    *) selected_color=$default 
+    echo "Return Value: $?";;
+esac
