8b40e86d376d0f555a2ee721e583966c3367dc6f
Ryan LaVigne <lavigner@pop-os.localdomain>
2020-10-13 20:41:43 -0400

    Updated with comments

--- color.sh
+++ color.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Author Ryan LaVigne
+# Date 10/13/2020
+
+# Color defaults
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
@@ -6,9 +10,11 @@ blue='\033[0;34m'
 magenta='\033[0;35m'
 default='\033[0m'
 
+# reads in what color
 read -p "What is your favorite color (red, green, blue, yellow, magenta)?" color
 color=${color^^}
 
+# case statement for whatever is typed in will do a function
 case $color in 
     "RED") selected_color=$red 
     echo -e "${selected_color}Your selected color is ${color}";;
